4dbd3f646c82f9d2456edf2fb86b34554f3af2f2
kenjanjan <kjliloc@hcdc.edu.ph>
2024-08-30 19:57:15 +0800

    initial commit 1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ export default function HomeScreen() {
   return (
     <SafeAreaView className="flex h-full w-full items-center justify-center bg-white">
       <Text className="text-red-500">
-        Open up App.js to start working on your app!
+        Hello World!
       </Text>
       <StatusBar />
     </SafeAreaView>
