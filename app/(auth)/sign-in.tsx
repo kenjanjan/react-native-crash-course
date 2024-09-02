0f139a3dfd97c87f93f5619951d6466ddc593c44
kntjhnllc <kentjohnliloc@gmail.com>
2024-09-02 14:40:15 +0800

    commit again

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SignIn = () => {
+  <SafeAreaView>
+    <Text>SignIn</Text>
+  </SafeAreaView>;
+};
+
+export default SignIn;
---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Onboarding = () => {
+  <SafeAreaView>
+    <Text>Onboarding</Text>
+  </SafeAreaView>;
+};
+
+export default Onboarding;
